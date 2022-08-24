d35c85f8ca5d8c48cc9f4456376a7ad96f76c6a6
PabloMarcial99 <17031116@itcelaya.edu.mx>
2022-08-24 18:30:07 -0500

    Avance

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,8 +4,16 @@ const Product = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen ">
-        <div className="w-5/6 bg-white p-2">
+        <div className="w-5/6 bg-white p-0">
           <img src={mobile} alt="" className="" />
+          <div className="p-5">
+            <h5 className="uppercase font-montserrat text-gray_blue">
+              Perfume
+            </h5>
+            <h2 className="font-bold font-fraunces text-2xl text-dark_blue">
+              Gabrielle Essence Eau De Parfum
+            </h2>
+          </div>
         </div>
       </div>
     </>
